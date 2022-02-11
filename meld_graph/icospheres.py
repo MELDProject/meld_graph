dfbbeb75a7f1cfb3a1a1f412a29eee83644b0667
kwagstyl <konrad.wagstyl@gmail.com>
2022-02-11 12:08:37 +0000

    icosphere coordinates and precalculation

--- meld_graph/icospheres.py
+++ meld_graph/icospheres.py
@@ -1,3 +1,4 @@
+#ico class
 import os
 import numpy as np
 import nibabel as nb
@@ -30,11 +31,21 @@ class IcoSpheres():
         
     def load_all_levels(self):
         for level in np.arange(7)+1:
-            self.load_icosphere(level = level)
-            self.calculate_edges(level = level)
-            self.calculate_neighbours(level = level)
-            self.spherical_coords(level = level)
-            self.pseudo_edge_attrs(level = level)
+            print(level)
+            self.load_one_level(level=level)
+            
+        return
+    
+    def load_one_level(self,level=7):
+        self.load_icosphere(level = level)
+        self.calculate_neighbours(level = level)
+        self.spherical_coords(level = level)
+        t1=time.time()
+        self.get_exact_edge_attrs(level=level)
+        t2=time.time()
+        print(t2-t1)
+        self.calculate_adj_mat(level=level)
+        
         return
         
     def load_icosphere(self,level=7):
@@ -43,13 +54,9 @@ class IcoSpheres():
               'faces':surf_nb.darrays[1].data}
         return 
     
-    def calculate_edges(self,level=7):
+    def calculate_adj_mat(self,level=7):
         surf=self.icospheres[level]
-        surf['edges'] = np.vstack(
-                [surf['faces'][:, :2], surf['faces'][:, 1:3], surf['faces'][:, [2, 0]],
-                 #add self edges
-                np.vstack([np.arange(len(surf['coords'])),np.arange(len(surf['coords']))]).T]
-            )
+        
         surf['adj_mat'] = sparse.coo_matrix(
                 (np.ones(len(surf['edges']), np.uint8), (surf['edges'][:, 0], surf['edges'][:, 1])),
                 shape=(len(surf["coords"]), len(surf["coords"])),
@@ -57,8 +64,13 @@ class IcoSpheres():
         return
     
     def calculate_neighbours(self,level=7):
-        self.icospheres[level]['neighbours'] = np.array(self.get_neighbours_from_tris(self.icospheres[level]['faces']),
+        file_path = os.path.join(self.icosphere_path,f'ico{level}.neighbours.npy')
+        if os.path.isfile(file_path):
+            self.icospheres[level]['neighbours'] = np.load(file_path,allow_pickle=True)
+        else:            
+            self.icospheres[level]['neighbours'] = np.array(self.get_neighbours_from_tris(self.icospheres[level]['faces']),
                                                         dtype=object)
+            np.save(file_path,self.icospheres[level]['neighbours'],allow_pickle=True)
         
         return
 
@@ -67,7 +79,7 @@ class IcoSpheres():
         self.icospheres[level]['spherical_coords'][:,0] = self.icospheres[level]['spherical_coords'][:,0] - pi/2
         return
     
-    def pseudo_edge_attrs(self,level=7):
+    def calculate_pseudo_edge_attrs(self,level=7):
         """pseudo edge attributes, difference between latitude and longitude"""
         col = self.icospheres[level]['edges'][:,0]
         row = self.icospheres[level]['edges'][:,1]
@@ -84,8 +96,7 @@ class IcoSpheres():
         alpha = np.remainder(alpha, 2*np.pi)
         pseudo[:,1]=alpha
         
-        self.icospheres[level]['pseudo_edge_attr'] = torch.from_numpy(pseudo)
-        self.icospheres[level]['edges'] = torch.from_numpy(self.icospheres[level]['edges'])
+        self.icospheres[level]['pseudo_edge_attr'] = pseudo
         return
     
    
@@ -95,12 +106,15 @@ class IcoSpheres():
         
         return self.icospheres[level]['edges']
     
-    def get_edge_vectors(self,level=7,dist_dtype ='pseudo'):
+    def get_edge_vectors(self,level=7,dist_dtype ='exact_edge_attr'):
         if dist_dtype == 'pseudo':
+            self.calculate_pseudo_edge_attrs(level = level)
             return self.icospheres[level]['pseudo_edge_attr']
         elif dist_dtype == 'exact':
             return self.icospheres[level]['exact_edge_attr']
 
+
+    
     def get_neighbours_from_tris(self,tris):
         """Get surface neighbours from tris
         Input: tris
@@ -109,18 +123,68 @@ class IcoSpheres():
         n_vert = np.max(tris) + 1
         neighbours = [[] for i in range(n_vert)]
         for tri in tris:
-            neighbours[tri[0]].extend([tri[1], tri[2]])
-            neighbours[tri[2]].extend([tri[0], tri[1]])
-            neighbours[tri[1]].extend([tri[2], tri[0]])
+            neighbours[tri[0]].append([tri[1], tri[2]])
+            neighbours[tri[2]].append([tri[0], tri[1]])
+            neighbours[tri[1]].append([tri[2], tri[0]])
         # Get unique neighbours
         for k in range(len(neighbours)):
-            neighbours[k] = [k]+self.f7(neighbours[k])
+            neighbours[k] = self.sort_neighbours(neighbours[k])
         return neighbours
+    
+    def sort_neighbours(self,edges):
+        edges=np.vstack(edges)
+        n0=edges[0][0]
+        sorted_neighbours=np.zeros(len(edges),dtype=int)
+        for e_i in np.arange(len(edges)):
+            n0 =  edges[:,1][edges[:,0]==n0][0]
+            sorted_neighbours[e_i]=n0
+        return sorted_neighbours
 
+    
+    def findAnglesBetweenTwoVectors1(self,v1s, v2s):
+        dot = np.einsum('ijk,ijk->ij',[v1s,v1s,v2s],[v2s,v1s,v2s])
+        return np.arccos(dot[0,:]/(np.sqrt(dot[1,:])*np.sqrt(dot[2,:])))
 
-    def f7(self,seq):
-        """returns uniques but in order of original appearance.
-        Used to retain neighbour triangle relationship"""
-        seen = set()
-        seen_add = seen.add
-        return [x for x in seq if not (x in seen or seen_add(x))]
+    def calculate_angles_and_dists(self,vertex,neighbours,coords):
+        angles=np.zeros(len(neighbours))
+        v1=coords[neighbours] - coords[vertex]
+        v2=coords[np.roll(neighbours,1)]-coords[vertex]
+        angles=findAnglesBetweenTwoVectors1(v1,v2)
+        total_angle=angles.sum()
+        angles_flattened = 2*pi*angles.cumsum()/total_angle
+        return angles_flattened, np.linalg.norm(v1,axis=1)
+
+    def vertex_attributes(self,surf,vertex):
+        neighbours=surf['neighbours'][vertex]
+        edges = neighbours_to_edges(vertex,neighbours)
+        angles,dists = calculate_angles_and_dists(vertex,neighbours,surf['coords'])
+        #add self edge with almost zero vals
+        edge_attrs=np.vstack([[1e-15,1e-15],np.vstack([angles,dists]).T])
+        combined=np.hstack([edges,edge_attrs])
+        return combined
+
+    def neighbours_to_edges(self,vertex,neighbours):
+        """generate paired ordered list of vertex to neighbours, including self edge"""
+        edges=np.vstack([[vertex,vertex],np.vstack([np.repeat(vertex,len(neighbours)),neighbours]).T])
+        return edges
+    
+    def get_exact_edge_attrs(self,level=7):
+        file_path = os.path.join(self.icosphere_path,f'ico{level}.edges_and_attrs.npy')
+        if os.path.isfile(file_path):
+            edges_attrs = np.load(file_path)
+        else:            
+            edges_attrs = self.calculate_exact_edge_attrs(level=level)
+            np.save(file_path,edges_attrs)
+        self.icospheres[level]['edges'] = edges_attrs[:,:2]
+        self.icospheres[level]['exact_edge_attr'] = edges_attrs[:,2:]
+        return
+    
+    def calculate_exact_edge_attrs(self,level=7):
+        surf = self.icospheres[level]
+        n_vert = len(surf['coords'])
+        all_edge_attrs=[]
+        for v in np.arange(n_vert):
+            edge_attrs=self.vertex_attributes(surf,v)
+            all_edge_attrs.append(edge_attrs)
+        all_edge_attrs = np.vstack(all_edge_attrs)
+        return all_edge_attrs
