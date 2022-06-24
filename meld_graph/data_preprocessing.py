05716927663e27561058dfb82c375c9b19ac3ead
kwagstyl <konrad.wagstyl@gmail.com>
2022-06-24 16:44:24 +0100

    synthetic data

--- meld_graph/data_preprocessing.py
+++ meld_graph/data_preprocessing.py
@@ -82,6 +82,8 @@ class Preprocess:
         else:
             pass
         return vals
+    
+    
 
     
     def get_data_preprocessed(self, subject, features, lobes=False, lesion_bias=False):
@@ -210,3 +212,47 @@ class Preprocess:
         with open(file, "w") as outfile:
             json.dump(x, outfile, indent=4)
         print(f"parameters saved in {file}")
+
+        
+    def clockwiseangle_and_distance(self,point):
+        import math
+
+        refvec = [0, 1]
+        # Vector between point and the origin: v = p - o
+        vector = [point[0]-self.origin[0], point[1]-self.origin[1]]
+        # Length of vector: ||v||
+        lenvector = math.hypot(vector[0], vector[1])
+        # If length is zero there is no angle
+        if lenvector == 0:
+            return -math.pi, 0
+        # Normalize vector: v/||v||
+        normalized = [vector[0]/lenvector, vector[1]/lenvector]
+        dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # x1*x2 + y1*y2
+        diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # x1*y2 - y1*x2
+        angle = math.atan2(diffprod, dotprod)
+        # Negative angles represent counter-clockwise angles so we need to subtract them 
+        # from 2*pi (360 degrees)
+        if angle < 0:
+            return 2*math.pi+angle, lenvector
+        # I return first the angle because that's the primary sorting criterium
+        # but if two vectors have the same angle then the shorter distance should come first.
+        return angle, lenvector
+
+    def generate_synthetic_data(self,coords,n_features,bias):
+        import matplotlib.path as mpltPath
+
+        com_i = np.random.choice(len(coords))
+        max_d = np.array([0.75,1.5])
+        n_points = np.random.choice(6)+4
+        shifted = coords-coords[com_i]
+        subset = coords[(np.abs(shifted)<max_d).all(axis=1)]
+        poly_i=np.random.choice(len(subset),n_points)
+        self.origin=coords[com_i]
+        polygon=subset[poly_i]
+        polygon=np.array(sorted(polygon, key=self.clockwiseangle_and_distance))
+
+        path = mpltPath.Path(polygon)
+        lesion = path.contains_points(coords)
+        n_verts=len(coords)
+        features = np.random.normal(0,1,(n_features,n_verts))+lesion.astype(int)*bias
+        return features, lesion
