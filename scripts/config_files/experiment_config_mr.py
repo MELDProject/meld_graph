19742f213122bb5560b258131827203cbf8ba5a1
mathrip <mathilde.ripart@gmail.com>
2022-06-24 17:45:47 +0100

    fix dice score

--- scripts/config_files/experiment_config_mr.py
+++ scripts/config_files/experiment_config_mr.py
@@ -2,28 +2,50 @@ import os, datetime
 
 # model and training parameters, passed to model and Trainer, respectively
 network_parameters = {
+    # MONETUNET
+    ## network_type: model class, one of: MoNet, MoNetUnet (see models.py)
+    #'network_type': 'MoNetUnet',
+    ## model_parameters: passed to model class initialiser
+    #'model_parameters': {
+    #    # model architecture: list of lists for Unet, and list for MoNet (simple convs)
+    #    'layer_sizes': [[32,32,32],[32,32,32],[64,64,64],[64,64,64],[128,128,128],[128,128,128],[256,256,256]],
+    #    # activation_fn: activation function, one of: relu, leaky_relu
+    #    'activation_fn': 'leaky_relu',
+    #    # conv_type: convolution to use, one of: SpiralConv, GMMConv.
+    #    'conv_type': 'SpiralConv',
+    #    # dim: coord dim for GMMConv
+    #    'dim': 2,
+    #    # kernel_size: number of gaussian kernels for GMMConv
+    #    'kernel_size': 3, # number of gaussian kernels
+    #    # spiral_len: size of the spiral for SpiralConv.
+    #    # TODO implement dilation / different spiral len per unet block
+    #    'spiral_len': 10, 
+    #},
+
+    # MONET
     # network_type: model class, one of: MoNet, MoNetUnet (see models.py)
-    'network_type': 'MoNetUnet',
+    'network_type': 'MoNet',
     # model_parameters: passed to model class initialiser
     'model_parameters': {
         # model architecture: list of lists for Unet, and list for MoNet (simple convs)
-        'layer_sizes': [[32,32,32],[32,32,32],[64,64,64],[64,64,64],[128,128,128],[128,128,128],[256,256,256]],
+        'layer_sizes': [],
         # activation_fn: activation function, one of: relu, leaky_relu
         'activation_fn': 'leaky_relu',
-        # conv_type: convolution to use, one of: SpiralConv, GMMConv. Only for MoNetUnet
+        # conv_type: convolution to use, one of: SpiralConv, GMMConv.
         'conv_type': 'SpiralConv',
         # dim: coord dim for GMMConv
         'dim': 2,
         # kernel_size: number of gaussian kernels for GMMConv
         'kernel_size': 3, # number of gaussian kernels
-        # spiral_len: size of the spiral for SpiralConv. Only for MoNetUnet
+        # spiral_len: size of the spiral for SpiralConv.
         # TODO implement dilation / different spiral len per unet block
-        'spiral_len': 10, 
+        'spiral_len': 1, 
     },
+
     # training_parameters: used by Trainer to set up model training
     'training_parameters': {
         "max_patience": 400,
-        "num_epochs": 800,
+        "num_epochs": 10,
         # optimiser: optimiser to use, one of: adam, sgd
         "optimiser": 'sgd',
         # optimiser_parameters: parameters passed to torch optimiser class
@@ -35,19 +57,19 @@ network_parameters = {
         },
         # lr_decay: exponent for exponential learning rate decay: lr*(1-epoch/max_epochs)**lr_decay
         # set to 0 to turn lr decay off
-        'lr_decay': 0,  # default NNUnet param: 0.9
+        'lr_decay': 0,
         # loss_dictionary: losses to be used for model training and parameters for losses
         # possible keys: cross_entropy, focal_loss, dice
         # values: dict with keys: "weight" and loss arguments (alpha/gamma for focal_loss, class_weights for dice)
         'loss_dictionary': {  
             #'cross_entropy':{'weight':1},
-            #'focal_loss':{'weight':1, 'alpha':0.4, 'gamma':4},
-            'dice':{'weight': 1, 'class_weights': [0.5, 0.5]}
+            'focal_loss':{'weight':1, 'alpha':0.4, 'gamma':4},
+            #'dice':{'weight': 1, 'class_weights': [0.5, 0.5]}
         },
          # metrics: list of metrics that should be printed during training
          # possible values: dice_lesion, dice_nonlesion, precision, recall, tp, fp, fn, tn
-        'metrics': ['dice_lesion', 'dice_nonlesion', 'precision', 'recall', 'tp', 'fp', 'fn'], 
-        "batch_size": 1,
+        'metrics': ['dice_lesion', 'dice_nonlesion', 'precision', 'recall', 'tp', 'fp', 'fn', 'tn'], 
+        "batch_size": 2,
         "shuffle_each_epoch": True,
         # deep_supervision: add loss at specified levels of the unet (for MoNetUnet).
         # Set to list of levels (eg [6,5,4]), for which to add output layers for additional supervision.
@@ -55,38 +77,40 @@ network_parameters = {
         'deep_supervision': {
             'levels': [], #[4,5,6], 
             'weight': 0.5
-        }
+        },
+        # ovesampling: oversample lesional vertices to 33% lesional and 66% random.
+        # size of epoch will be num_lesional_examples * 3
+        'oversampling':True,
     },
     # name: experiment name. If none, experiment is not saved
-    'name': datetime.datetime.now().strftime("%y-%m-%d") + '_example',
+    'name': datetime.datetime.now().strftime("%y-%m-%d") + '_lr_decay',
 }
-
 # data parameters, passed to GraphDataset and Preprocess
 data_parameters = {
     'hdf5_file_root': "{site_code}_{group}_featurematrix_combat_6.hdf5",
     'site_codes': [
-        "H1",
-        "H2",
-        "H3",
+#         "H1",
+#         "H2",
+#         "H3",
         "H4",
-    #    "H5",
-    #    "H6",
-    #    "H7",
-    #    "H9",
-    #    "H10",
-    #    "H11",
-    #    "H12",
-    #    "H14",
-    #    "H15",
-    #    "H16",
-    #    "H17",
-    #    "H18",
-    #    "H19",
-    #    "H21",
-    #    "H23",
-    #    "H24",
-    #    "H26",
-    #    "H27",
+#        "H5",
+#        "H6",
+#        "H7",
+#        "H9",
+#        "H10",
+#        "H11",
+#        "H12",
+#        "H14",
+#        "H15",
+#        "H16",
+#        "H17",
+#        "H18",
+#        "H19",
+#        "H21",
+#        "H23",
+#        "H24",
+#        "H26",
+#        "H27",
     ],
     'scanners': ['15T','3T'],
     'dataset': 'MELD_dataset_V6.csv',
@@ -94,7 +118,8 @@ data_parameters = {
     "features_to_exclude": [],
     "subject_features_to_exclude": [],
     # features: manually specify features (instead of features_to_exclude)
-    "features": [#'.on_lh.lesion.mgh',
+    "features": [
+        '.on_lh.lesion.mgh',
         #    '.on_lh.curv.mgh',
         #    '.on_lh.gm_FLAIR_0.25.mgh',
         #    '.on_lh.gm_FLAIR_0.5.mgh',
@@ -106,39 +131,39 @@ data_parameters = {
         #    '.on_lh.w-g.pct.mgh',
         #    '.on_lh.wm_FLAIR_0.5.mgh',
         #    '.on_lh.wm_FLAIR_1.mgh',
-        '.combat.on_lh.pial.K_filtered.sm20.mgh',
+#         '.combat.on_lh.pial.K_filtered.sm20.mgh',
         '.combat.on_lh.thickness.sm10.mgh',
-        '.combat.on_lh.w-g.pct.sm10.mgh',
-        '.combat.on_lh.sulc.sm5.mgh',
-        '.combat.on_lh.curv.sm5.mgh',
-        '.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
-        '.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
-        '.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
-        '.combat.on_lh.gm_FLAIR_0.sm10.mgh',
-        '.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
-        '.combat.on_lh.wm_FLAIR_1.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.pial.K_filtered.sm20.mgh',
-        '.inter_z.intra_z.combat.on_lh.thickness.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.w-g.pct.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.sulc.sm5.mgh',
-        '.inter_z.intra_z.combat.on_lh.curv.sm5.mgh',
-        '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
-        '.inter_z.intra_z.combat.on_lh.wm_FLAIR_1.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.pial.K_filtered.sm20.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.thickness.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.w-g.pct.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.sulc.sm5.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.curv.sm5.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
-        '.inter_z.asym.intra_z.combat.on_lh.wm_FLAIR_1.sm10.mgh',
+#         '.combat.on_lh.w-g.pct.sm10.mgh',
+#         '.combat.on_lh.sulc.sm5.mgh',
+#         '.combat.on_lh.curv.sm5.mgh',
+#         '.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
+#         '.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
+#         '.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
+#         '.combat.on_lh.gm_FLAIR_0.sm10.mgh',
+#         '.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
+#         '.combat.on_lh.wm_FLAIR_1.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.pial.K_filtered.sm20.mgh',
+#         '.inter_z.intra_z.combat.on_lh.thickness.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.w-g.pct.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.sulc.sm5.mgh',
+#         '.inter_z.intra_z.combat.on_lh.curv.sm5.mgh',
+#         '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.gm_FLAIR_0.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
+#         '.inter_z.intra_z.combat.on_lh.wm_FLAIR_1.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.pial.K_filtered.sm20.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.thickness.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.w-g.pct.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.sulc.sm5.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.curv.sm5.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.75.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.5.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.25.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.gm_FLAIR_0.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.wm_FLAIR_0.5.sm10.mgh',
+#         '.inter_z.asym.intra_z.combat.on_lh.wm_FLAIR_1.sm10.mgh',
     ],
     # specify this if manually specifying features
     "features_to_replace_with_0": [], 
@@ -148,7 +173,7 @@ data_parameters = {
     "preprocessing_parameters": {
         "scaling": None, #"scaling_params_GDL.json"
         # zscore: normalise all values (per subject)
-        "zscore": True,
+        "zscore": False,
     },
     # icosphere_parameters: passed to Icospheres class
     "icosphere_parameters": {
@@ -158,20 +183,21 @@ data_parameters = {
     # augment_data: parameters passed to Augment class
     # dictionary containing augmentation method as keys, and Transform params as values ("p" and "file")
     # possible augmentation methods: spinning, warping, flipping
-    "augment_data": {
-        "spinning":
-                  {'p': 0.80,
-                  'file': 'data/spinning/spinning_ico7_10.npy'
-                  },
-        "warping": 
-                  {'p': 0.80,
-                   'file': 'data/warping/warping_ico7_10.npy'
-                  },
-        "flipping":
-                  {'p': 0.80,
-                   'file': 'data/flipping/flipping_ico7_3.npy'
-                  },
-                    },
+    "augment_data": None,
+#     {
+#         "spinning":
+#                   {'p': 0.80,
+#                   'file': 'data/spinning/spinning_ico7_10.npy'
+#                   },
+#         "warping": 
+#                   {'p': 0.80,
+#                    'file': 'data/warping/warping_ico7_10.npy'
+#                   },
+#         "flipping":
+#                   {'p': 0.80,
+#                    'file': 'data/flipping/flipping_ico7_3.npy'
+#                   },
+#                     },
     # combine_hemis: how to combine hemisphere data, one of: None, stack
     # None: no combination of hemispheres. 
     # "stack": stack features of both hemispheres.
