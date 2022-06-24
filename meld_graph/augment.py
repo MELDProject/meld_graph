f7e912688eba4d4111485e7bab0e39130718e9e4
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2022-06-24 10:45:58 +0100

    fix files dropped during merge

--- meld_graph/augment.py
+++ meld_graph/augment.py
@@ -13,32 +13,6 @@ from meld_graph.paths import (
     SCRIPTS_DIR,)
 
 
-#class Transform():
-#    """Class transform paramaters"""
-#    def __init__(self, params_transform):
-#        self.p = params_transform['p']
-#        self.lambdas, self.indices = np.load(os.path.join(SCRIPTS_DIR,params_transform['file']))
-#        self.indices = self.indices.astype('int') 
-        
-#    def apply_transform(self, feats, lesions=None):
-#        # select random transformation parameter
-#        transf = np.random.randint(0,len(self.lambdas))
-#        # spin lesions if exist
-#        if lesions.any()!= None:
-#            lesions_transf = self.lambdas[transf,:,0]*lesions[self.indices[transf,:,0]] + self.lambdas[transf,:,1]*lesions[self.indices[transf,:,1]] + self.lambdas[transf,:,2]*lesions[self.indices[transf,:,2]]   
-#            lesions_transf = np.round(lesions_transf)
-#        # spin features
-#        n_feat = len(feats.T)
-#        lambdas = np.tile(self.lambdas[:,:,:,np.newaxis], n_feat )
-#        feats_transf = lambdas[transf,:,0]*feats[self.indices[transf,:,0]] + lambdas[transf,:,1]*feats[self.indices[transf,:,1]] + lambdas[transf,:,2]*feats[self.indices[transf,:,2]]        
-#        feats_transf_clean=np.zeros(feats_transf.shape)
-##         for i in range(0,n_feat):
-##             feats_transf_clean[:,i]=np.clip(feats_transf[:,i], np.percentile(feats_transf[:,i], 0.01),np.percentile(feats_transf[:,i], 99.9))  
-#        return feats_transf_clean, lesions_transf
-
-
-
-
 class Transform():
     """Class transform paramaters"""
     def __init__(self, params_transform):
