7436b01753b4f7ef17cdf40fb40dbc58cbe86c59
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2023-06-12 11:36:58 +0100

    add version and authors to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
+try:
+    from meld_graph import __author__, __maintainer__, __email__, __version__
+except ImportError:
+    __author__ = __maintainer__ = "MELD development team"
+    __email__ = "meld.study@gmail.com"
+    __version__ = "0.0.0"
+
 setup(
     name="meld_graph",
-    version="0.1",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__author__,
+    maintainer_email=__email__,
+    description="Graph-based lesion segmentation for the MELD project.",
+    license="MIT",
     packages=find_packages(),
     install_requires=[],
     package_dir={"meld_graph": "meld_graph"},
