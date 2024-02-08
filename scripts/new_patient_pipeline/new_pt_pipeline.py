07b8e28315d7300b718fd7c9865f5448226f1e2e
mathrip <mathilde.ripart@gmail.com>
2024-02-08 15:17:38 +0000

    fix issue subject id number

--- scripts/new_patient_pipeline/new_pt_pipeline.py
+++ scripts/new_patient_pipeline/new_pt_pipeline.py
@@ -28,8 +28,8 @@ def create_demographic_file(subjects_ids, save_file, harmo_code='noHarmo'):
         df = pd.DataFrame()
         if  isinstance(subjects_ids, str):
             subjects_ids=[subjects_ids]
-        df['ID']=subjects_ids
-        df['Harmo code']=[harmo_code for subject in subjects_ids]
+        df['ID']=subjects_ids.astype(str)
+        df['Harmo code']=[str(harmo_code) for subject in subjects_ids]
         df['Group']=['patient' for subject in subjects_ids]
         df['Scanner']=['3T' for subject in subjects_ids]
         df.to_csv(save_file)
