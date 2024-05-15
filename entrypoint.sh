413f032078fa45256461315a4c28732c3c0b6dd8
Ulysses Popple <ulysses.popple@gmail.com>
2024-05-15 08:34:52 +0100

    remove activate line from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Activate the Conda environment
-source activate meld_graph
-
 # Run your application
 # exec gosu myuser python "scripts/new_patient_pipeline/$@"
 # python scripts/new_patient_pipeline/$@
