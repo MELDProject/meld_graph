53270d1f3c7a7fe73d6ab8d1a854ec786c061a7f
mathrip <mathilde.ripart@gmail.com>
2024-09-24 14:56:48 +0100

    fix issue which not available on singularity

--- scripts/env_setup.py
+++ scripts/env_setup.py
@@ -1,13 +1,13 @@
 import os,sys,json,subprocess
 
 def setup():
-    from meld_graph.paths import MELD_DATA_PATH
+    # from meld_graph.paths import MELD_DATA_PATH
     if not "FREESURFER_HOME" in os.environ:
         if sys.platform == "darwin":
             if os.path.exists("/Applications/freesurfer/7.2.0"):
                 os.environ["FREESURFER_HOME"] = "/Applications/freesurfer/7.2.0"
         
-    freesurfercheck = subprocess.run(['/bin/bash', '-c', "which freeview"], capture_output=True)
+    freesurfercheck = subprocess.run(['/bin/bash', '-c', "type freeview"], capture_output=True)
     if freesurfercheck.returncode > 0:
         source = 'source /Applications/freesurfer/7.2.0/SetUpFreeSurfer.sh'
         dump = 'python -c "import os,json;print(json.dumps(dict(os.environ)))"'
@@ -15,10 +15,10 @@ def setup():
         env = json.loads(penv.stdout)
         os.environ.update(env)
     if not "FS_LICENSE" in os.environ:
-        if os.path.exists(f"{MELD_DATA_PATH}/license.txt"):
-            print("setting license" + f"{MELD_DATA_PATH}/license.txt")
-            os.environ["FS_LICENSE"] = f"{MELD_DATA_PATH}/license.txt"
-        elif os.path.exists(f"{os.getcwd()}/license.txt"):
+        # if os.path.exists(f"{MELD_DATA_PATH}/license.txt"):
+        #     print("setting license" + f"{MELD_DATA_PATH}/license.txt")
+        #     os.environ["FS_LICENSE"] = f"{MELD_DATA_PATH}/license.txt"
+        if os.path.exists(f"{os.getcwd()}/license.txt"):
             print("setting license" + f"{os.getcwd()}/license.txt")
             os.environ["FS_LICENSE"] = f"{os.getcwd()}/license.txt"
         else:
