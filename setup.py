6acf4e9a99d29d52942c7366eb7c4261ecd91199
mathrip <mathilde.ripart@gmail.com>
2025-10-20 15:15:30 +0100

    update version v2.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     __author__ = __maintainer__ = "MELD development team"
     __email__ = "meld.study@gmail.com"
-    __version__ = "2.2.2"
+    __version__ = "2.2.3"
 
 setup(
     name="meld_graph",
